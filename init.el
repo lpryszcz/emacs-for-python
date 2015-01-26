51b199d0c35887325c366173b5565e09b50e79dd
Leszek Pryszcz <lpryszcz@i7-k4770>
2015-01-26 17:12:33 +0100

    fresh repo cleaned

--- init.el
+++ init.el
@@ -1,3 +1,11 @@
+;; markdown mode http://jblevins.org/projects/markdown-mode/
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
 ;; Add my personal directory to the path 
 (setq load-path (cons "~/.emacs.d/" load-path))
 
