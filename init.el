349fce5c683366aec6e0cc31f4a233016e3e5adb
Leszek <lpryszcz@crg.es>
2019-07-19 15:13:55 +0200

    solved eldoc error: (void-variable comint-last-prompt-overlay)

--- init.el
+++ init.el
@@ -135,44 +135,6 @@
              (cons tramp-file-name-regexp nil))
 
 
-;;  COMPLETION
-
-;; ;; list of expansion functions tried (in order) by `hippie-expand'
-;; (setq hippie-expand-try-functions-list
-;;       '(try-expand-dabbrev   ; from current buffer
-;;         try-expand-dabbrev-visible   ; from visible parts of all windows
-;;         try-expand-dabbrev-all-buffers   ; from all other buffers
-;;         try-expand-dabbrev-from-kill
-;;         try-complete-file-name-partially
-;;         try-complete-file-name
-;;         try-expand-all-abbrevs
-;;         try-expand-list
-;;         try-expand-line
-;;         try-complete-lisp-symbol-partially
-;;         try-complete-lisp-symbol
-;;         try-expand-whole-kill))
-
-;; ;; expand-function
-;; (defun my-hippie-expand (arg)
-;;   ;; called with a positive prefix `P', it jumps directly to the `P'-th
-;;   ;; `try-function'
-;;   (interactive "P")
-;;   ;; `hippie-expand' does not have a customization-feature (like
-;;   ;; `dabbrev-expand') to search case-sensitive for completions. So we
-;;   ;; must set `case-fold-search' temporarily to nil!
-;;   (let ((old-case-fold-search case-fold-search))
-;;     (setq case-fold-search nil)
-;;     (hippie-expand arg)
-;;     (setq case-fold-search old-case-fold-search)))
-
-;; (global-set-key [(S-iso-lefttab)] 'my-hippie-expand)
-;; (global-set-key [(backtab)] 'my-hippie-expand)
-;; ;(global-set-key [(control tab)] 'my-hippie-expand)
-
-;; ; Shift-TAB does not work in terminal mode. In my case, backtab is recognized instead
-;; (global-set-key [backtab] 'my-hippie-expand)
-
-
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
